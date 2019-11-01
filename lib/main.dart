05c8692ad428603f663e0b1c71cb335dd13d17e8
Rodrigo Dopazo Iglesias <>
2019-11-01 11:03:59 +0100

    [ACUDIA-8] Added translations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:acudia/core/localizations/app-localizations.dart';
 import 'package:acudia/core/providers/CounterProvider.dart';
 import 'package:acudia/ui/screens/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(ChangeNotifierProvider(
@@ -16,6 +18,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('ar', ''),
+      ],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
