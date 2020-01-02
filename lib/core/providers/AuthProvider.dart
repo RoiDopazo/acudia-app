b6e968e30573014ac5da4e216480d37ce652ba02
Rodrigo Dopazo Iglesias <r.a.dopazo.iglesias@accenture.com>
2020-01-02 17:27:01 +0100

    [ACUDIA-12] Getting token from google login

--- lib/core/providers/AuthProvider.dart
+++ lib/core/providers/AuthProvider.dart
@@ -1,25 +1,21 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthProvider {
-  final GoogleSignIn _googleSignIn = GoogleSignIn(
-      scopes: ['email', 'https://www.googleapis.com/auth/contacts.readonly']);
-
-  initLogin() {
-    _googleSignIn.onCurrentUserChanged
-        .listen((GoogleSignInAccount account) async {
-      if (account != null) {
-        print('hello');
-        // user logged
-      } else {
-        print('not logged');
-        // user NOT logged
-      }
-    });
-    _googleSignIn.signInSilently().whenComplete(() => print('when compleete'));
-  }
+  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: [
+    'email',
+    'https://www.googleapis.com/auth/user.birthday.read',
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ]);
 
   doLogin() async {
     print('doLogin');
-    await _googleSignIn.signIn();
+    final GoogleSignInAccount googleSignInAccount =
+        await _googleSignIn.signIn();
+
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    print(googleSignInAuthentication.accessToken);
+    print(googleSignInAuthentication.idToken);
   }
 }
