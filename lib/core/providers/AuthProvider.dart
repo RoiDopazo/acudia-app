6004b217a1e529732407033d1de8014749f34ced
Rodrigo Dopazo Iglesias <r.a.dopazo.iglesias@accenture.com>
2020-05-09 12:35:34 +0200

    AppSync - google auth

--- lib/core/providers/AuthProvider.dart
+++ lib/core/providers/AuthProvider.dart
@@ -1,3 +1,5 @@
+import 'package:acudia/core/aws.dart';
+import 'package:acudia/core/google.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthProvider {
@@ -8,14 +10,30 @@ class AuthProvider {
   ]);
 
   doLogin() async {
-    print('doLogin');
-    final GoogleSignInAccount googleSignInAccount =
-        await _googleSignIn.signIn();
+    final googleSignInAuthentication = await signInWithGoogle();
+    print(googleSignInAuthentication);
+    final credentials = new Credentials(
+      'eu-west-1:2656d5c4-c95d-4a4e-9ffc-584b0d97d9cc',
+      'eu-west-1_pI6G9Yvog',
+      '61hus629iapedbilpgk438qup5',
+      googleSignInAuthentication.idToken,
+      'accounts.google.com',
+    );
 
-    final GoogleSignInAuthentication googleSignInAuthentication =
-        await googleSignInAccount.authentication;
+    final api =
+        Api('http://localhost:8000', '/flutter', 'eu-west-1', credentials);
 
-    print(googleSignInAuthentication.accessToken);
-    print(googleSignInAuthentication.idToken);
+    final result = await api.post({});
+
+    print(result.body);
+    // print('doLogin');
+    // final GoogleSignInAccount googleSignInAccount =
+    //     await _googleSignIn.signIn();
+
+    // final GoogleSignInAuthentication googleSignInAuthentication =
+    //     await googleSignInAccount.authentication;
+
+    // print(googleSignInAuthentication.accessToken);
+    // print(googleSignInAuthentication.idToken);
   }
 }
