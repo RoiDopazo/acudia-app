885bd11ae52f76670495dedd8afd95b178aab56b
rdopazo <rdopazobucket@gmail.com>
2020-08-17 15:34:32 +0200

    [ACUDIA-17] Cloudinary integration

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -6,6 +6,12 @@ final String AWS_APP_SYNC_ENDPOINT =
 final String USER_POOL_ID = Environment.getEnvValueForKey('USER_POOL_ID');
 final String USER_POOL_CLIENT_ID =
     Environment.getEnvValueForKey('USER_POOL_CLIENT_ID');
+final String CLOUDINARY_API_KEY =
+    Environment.getEnvValueForKey('CLOUDINARY_API_KEY');
+final String CLOUDINARY_API_SECRET =
+    Environment.getEnvValueForKey('CLOUDINARY_API_SECRET');
+final String CLOUDINARY_API_NAME =
+    Environment.getEnvValueForKey('CLOUDINARY_API_NAME');
 
 enum USER_ROLES { client, acudier }
 enum USER_GENDER { male, female, other }
