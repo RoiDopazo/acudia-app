8526f6ea40bdb5b854e7cbc579bca23259670d68
rdopazo <rdopazobucket@gmail.com>
2020-10-18 12:00:46 +0200

    [ACUDIA-38] Added nested scroll

--- lib/ui/screens/main/hospital/details/hospital_details_page.dart
+++ lib/ui/screens/main/hospital/details/hospital_details_page.dart
@@ -14,76 +14,75 @@ class HospitalDetailsPage extends StatelessWidget {
     print(MediaQuery.of(context).size.width);
 
     return Consumer<HospitalProvider>(
-      builder: (context, hospProvider, child) => DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            body: CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  collapsedHeight: 52,
-                  toolbarHeight: 50,
-                  expandedHeight: 240.0,
-                  floating: false,
-                  pinned: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                    title: LayoutBuilder(builder: (context, size) {
-                      var span = TextSpan(
-                          text: args.hospital.name, style: TextStyle(color: Theme.of(context).scaffoldBackgroundColor));
+        builder: (context, hospProvider, child) => DefaultTabController(
+            length: 2,
+            child: Scaffold(
+              body: NestedScrollView(
+                  headerSliverBuilder: (context, value) {
+                    return [
+                      SliverAppBar(
+                        collapsedHeight: 52,
+                        toolbarHeight: 50,
+                        expandedHeight: 240.0,
+                        floating: false,
+                        pinned: true,
+                        flexibleSpace: FlexibleSpaceBar(
+                          title: LayoutBuilder(builder: (context, size) {
+                            var span = TextSpan(
+                                text: args.hospital.name,
+                                style: TextStyle(color: Theme.of(context).scaffoldBackgroundColor));
 
-                      return Text.rich(
-                        span,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: size.maxHeight > 180 ? 3 : 1,
-                      );
-                    }),
-                  ),
-                ),
-                SliverPersistentHeader(
-                  pinned: true,
-                  delegate: SliverAppBarDelegate(
-                    minHeight: 68.0,
-                    maxHeight: 68.0,
-                    child: Container(
-                      color: Theme.of(context).scaffoldBackgroundColor,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 10.0,
-                            ),
-                            child: TabBar(
-                              tabs: [
-                                Tab(child: Text('Búsqueda')),
-                                Tab(child: Text('Detalles')),
+                            return Text.rich(
+                              span,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: size.maxHeight > 180 ? 3 : 1,
+                            );
+                          }),
+                        ),
+                      ),
+                      SliverPersistentHeader(
+                        pinned: true,
+                        delegate: SliverAppBarDelegate(
+                          minHeight: 60.0,
+                          maxHeight: 60.0,
+                          child: Container(
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                            child: Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(top: 10),
+                                  child: TabBar(
+                                    tabs: [
+                                      Tab(child: Text('Búsqueda')),
+                                      Tab(child: Text('Detalles')),
+                                    ],
+                                  ),
+                                ),
                               ],
                             ),
                           ),
-                          // Divider(),
+                        ),
+                      )
+                    ];
+                  },
+                  body: ScrollConfiguration(
+                      behavior: CustomScrollConfig(),
+                      child: Container(
+                          child: TabBarView(
+                        children: [
+                          SingleChildScrollView(
+                              child: Column(children: [
+                            Text('3213123'),
+                            SizedBox(height: 400),
+                            Text('3213123'),
+                            SizedBox(height: 400),
+                            Text('3213123'),
+                            SizedBox(height: 400)
+                          ])),
+                          Container(child: Icon(Icons.directions_transit)),
                         ],
-                      ),
-                    ),
-                  ),
-                ),
-                SliverToBoxAdapter(
-                    child: Column(children: [
-                  Text('asdas'),
-                  SizedBox(height: 400),
-                  Text('asdas'),
-                  SizedBox(height: 400),
-                  Text('asdas'),
-                  SizedBox(height: 400)
-                ]))
-                // SliverList(
-                //   delegate: SliverChildBuilderDelegate((context, index) {
-                //     return ListTile(
-                //       title: Text('List Tile $index'),
-                //     );
-                //   }, childCount: 100),
-                // ),
-              ],
-            ),
-          )),
-    );
+                      )))),
+            )));
   }
 }
 
@@ -109,5 +108,12 @@ class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
   @override
   bool shouldRebuild(SliverAppBarDelegate oldDelegate) {
     return maxHeight != oldDelegate.maxHeight || minHeight != oldDelegate.minHeight || child != oldDelegate.child;
+  }
+}
+
+class CustomScrollConfig extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
   }
 }
