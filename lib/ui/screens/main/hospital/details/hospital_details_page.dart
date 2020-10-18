af9a3cd7b534bb8940258c79937b5c3d85db6a48
rdopazo <rdopazobucket@gmail.com>
2020-10-18 11:41:31 +0200

    [ACUDIA-38] SliderAppBar with fixed tabs

--- lib/ui/screens/main/hospital/details/hospital_details_page.dart
+++ lib/ui/screens/main/hospital/details/hospital_details_page.dart
@@ -3,6 +3,7 @@ import 'package:acudia/ui/screens/main/hospital/details/hospital_details_args.da
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'dart:math' as math;
 
 class HospitalDetailsPage extends StatelessWidget {
   @override
@@ -13,39 +14,100 @@ class HospitalDetailsPage extends StatelessWidget {
     print(MediaQuery.of(context).size.width);
 
     return Consumer<HospitalProvider>(
-      builder: (context, hospProvider, child) => Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              collapsedHeight: 52,
-              toolbarHeight: 50,
-              expandedHeight: 180.0,
-              floating: false,
-              pinned: true,
-              backgroundColor: Theme.of(context).accentColor,
-              flexibleSpace: FlexibleSpaceBar(
-                title: LayoutBuilder(builder: (context, size) {
-                  var span = TextSpan(
-                      text: args.hospital.name, style: TextStyle(color: Theme.of(context).scaffoldBackgroundColor));
+      builder: (context, hospProvider, child) => DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            body: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  collapsedHeight: 52,
+                  toolbarHeight: 50,
+                  expandedHeight: 240.0,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: LayoutBuilder(builder: (context, size) {
+                      var span = TextSpan(
+                          text: args.hospital.name, style: TextStyle(color: Theme.of(context).scaffoldBackgroundColor));
 
-                  return Text.rich(
-                    span,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: size.maxHeight > 180 ? 3 : 1,
-                  );
-                }),
-              ),
-            ),
-            SliverFillRemaining(
-              child: Center(
-                child: Column(
-                  children: [Text('dddddd'), Text('ggggggg')],
+                      return Text.rich(
+                        span,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: size.maxHeight > 180 ? 3 : 1,
+                      );
+                    }),
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+                SliverPersistentHeader(
+                  pinned: true,
+                  delegate: SliverAppBarDelegate(
+                    minHeight: 68.0,
+                    maxHeight: 68.0,
+                    child: Container(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10.0,
+                            ),
+                            child: TabBar(
+                              tabs: [
+                                Tab(child: Text('Búsqueda')),
+                                Tab(child: Text('Detalles')),
+                              ],
+                            ),
+                          ),
+                          // Divider(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                    child: Column(children: [
+                  Text('asdas'),
+                  SizedBox(height: 400),
+                  Text('asdas'),
+                  SizedBox(height: 400),
+                  Text('asdas'),
+                  SizedBox(height: 400)
+                ]))
+                // SliverList(
+                //   delegate: SliverChildBuilderDelegate((context, index) {
+                //     return ListTile(
+                //       title: Text('List Tile $index'),
+                //     );
+                //   }, childCount: 100),
+                // ),
+              ],
+            ),
+          )),
     );
+  }
+}
+
+class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  SliverAppBarDelegate({
+    @required this.minHeight,
+    @required this.maxHeight,
+    @required this.child,
+  });
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  @override
+  double get minExtent => minHeight;
+  @override
+  double get maxExtent => math.max(maxHeight, minHeight);
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight || minHeight != oldDelegate.minHeight || child != oldDelegate.child;
   }
 }
