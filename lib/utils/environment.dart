69e2907840178bedf9059084a66e99e6c0a3e12b
rdopazo <rdopazobucket@gmail.com>
2021-11-22 11:31:58 +0100

    Environment update

--- lib/utils/environment.dart
+++ lib/utils/environment.dart
@@ -3,9 +3,9 @@ import 'dart:async';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
-  static const ENV_LOCAL_FILE_NAME = '.env';
+  static const ENV_LOCAL_FILE_NAME = '.env.local';
   static const ENV_DEV_FILE_NAME = '.env.development';
-  static const ENV_PROD_FILE_NAME = '.env.production';
+  static const ENV_PROD_FILE_NAME = '.env';
 
   const Environment();
 
@@ -16,11 +16,13 @@ class Environment {
   }
 
   static Future<void> loadEnvFile() async {
+    const IS_LOCAL = String.fromEnvironment('LOCAL', defaultValue: 'false');
     const IS_DEV = String.fromEnvironment('DEV', defaultValue: 'false');
     const IS_PROD = String.fromEnvironment('PROD', defaultValue: 'false');
 
+    if (IS_LOCAL == 'true') return await DotEnv().load(ENV_LOCAL_FILE_NAME);
     if (IS_PROD == 'true') return await DotEnv().load(ENV_PROD_FILE_NAME);
     if (IS_DEV == 'true') return await DotEnv().load(ENV_DEV_FILE_NAME);
-    return await DotEnv().load(ENV_LOCAL_FILE_NAME);
+    return await DotEnv().load(ENV_PROD_FILE_NAME);
   }
 }
